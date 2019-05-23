5dda635ee2e9ec1e0d246688bbabe9927c600824
soulrelic616 <fataldesign616@gmail.com>
2019-05-23 10:10:41 +0100

    click on model now working

--- public/scripts.js
+++ public/scripts.js
@@ -17,9 +17,9 @@ camera.position.z = 9.867664273831625;
 */
 
 
-camera.position.x = 0;
-camera.position.y = 400;
-camera.position.z = 800;
+camera.position.x = 23.35238695426168;
+camera.position.y = 8.717253467074778;
+camera.position.z = -17.80472949084262;
 camera.lookAt(scene.position);
 
 
@@ -65,17 +65,19 @@ objLoader.load('WALKERS.obj', function(object){
 });*/
 
 var mtlLoader = new THREE.MTLLoader();
-mtlLoader.setResourcePath('models/');
-mtlLoader.setPath('models/');
-mtlLoader.load('WALKERS.mtl', function (materials) {
+
+/*LOAD AT ACT*/
+mtlLoader.setResourcePath('models/at-act/');
+mtlLoader.setPath('models/at-act/');
+mtlLoader.load('AT-ACT.mtl', function (materials) {
     materials.preload();
     
     var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials(materials);
-    objLoader.setPath('models/');
-    objLoader.load('WALKERS.obj', function(object){
+    objLoader.setPath('models/at-act/');
+    objLoader.load('AT-ACT.obj', function(object){
         //object.position.y -= 60;
-        object.userData.name = "WALKERS";
+        object.userData.name = "at-act";
         scene.add(object);
         objects.push(object);
     });
@@ -84,36 +86,85 @@ mtlLoader.load('WALKERS.mtl', function (materials) {
 
 var objects = [];
 
-function onDocumentMouseDown( event ){
+var geometry, material, mesh, INTERSECTED;
+
+var container = document.body;
+
+var mouseX, MouseY, vector;
+
+
+function actionFn( event ){
     event.preventDefault();
     var mouseX = (event.clientX / window.innerWidth)*2-1;
     var mouseY = -(event.clientY /window.innerHeight)*2+1;
     var vector = new THREE.Vector3( mouseX, mouseY, 0.5 );
     //projector.unprojectVector( vector, camera );
     
-    
     var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+    
+    /*var raycaster = new THREE.Raycaster();
+    
+    raycaster.setFromCamera( new THREE.Vector2(), camera );  */
+    
     var intersects = raycaster.intersectObjects( objects, true );
-
-    geometry.computeFaceNormals();
-
     
     //console.log( intersects[0].point);
-    console.log(intersects);
-    //console.log(objects[0].userData.name);
+    //console.log(intersects[0]);
     //console.log(objects[0].userData.name);
     //console.log(objects);
+    
+    if (intersects.length > 0) {
+        if (INTERSECTED != intersects[0].object) {
+            if (INTERSECTED) INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+            INTERSECTED = intersects[0].object;
+            INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+            INTERSECTED.material.color.setHex(130000);
+            
+            /*scene.add(new THREE.ArrowHelper( raycaster.ray.direction, raycaster.ray.origin, 100, Math.random() * 0xffffff ));*/
+            
+        }
+        container.style.cursor = 'pointer';
+    } else {
+        if (INTERSECTED) INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+        INTERSECTED = null;
+        container.style.cursor = 'auto';
+    }
+    geometry.computeFaceNormals();
 
-    scene.add(new THREE.ArrowHelper( raycaster.ray.direction, raycaster.ray.origin, 100, Math.random() * 0xffffff ));
-    
-    
 }
 
+// find intersections
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+// mouse listener
+document.addEventListener( 'mousedown', function( event ) {
+
+    var rect = renderer.domElement.getBoundingClientRect();
+    mouse.x = ( ( event.clientX - rect.left ) / ( rect.width - rect.left ) ) * 2 - 1;
+    mouse.y = - ( ( event.clientY - rect.top ) / ( rect.bottom - rect.top) ) * 2 + 1;
+
+    raycaster.setFromCamera( mouse, camera );
+
+    var intersects = raycaster.intersectObjects( objects, true );
+
+    console.log(intersects);
+    
+    if ( intersects.length > 0 ) {
+
+        //info.innerHTML = 'INTERSECT Count: ' + ++count;
+        //console.log('no intersects');
+
+    }
+    
+    console.log('click');
+
+}, false );
 
 
-$('body').on('click', function(e){
-    onDocumentMouseDown(e);
-    //console.log(objects);
+$('body').on('mousemove', function(e){
+    //actionFn(e);
+    //console.log(objects); 
 });
 
 
@@ -121,9 +172,6 @@ var animate = function () {
 	requestAnimationFrame( animate );
 
     controls.update;
-    
-	/*cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;*/
 
 	renderer.render(scene, camera);
     
